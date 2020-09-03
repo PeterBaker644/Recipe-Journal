795e622599a50cc2c22eff59522c2f1f50521509
Blackstar <baker644@umn.edu>
2020-09-02 21:12:55 -0500

    Backend authentication is now running. Currently authenticates every api request, but this can change

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Splash from "./pages/splash/Splash";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
-<<<<<<< HEAD
-import Gene from "./pages/Gene";
+import Gene from "./pages/gene/Gene";
 import { AuthProvider } from "./component/Auth";
 import PrivateRoute from "./component/PrivateRoute";
-=======
-import Gene from "./pages/gene/Gene";
->>>>>>> origin
 
 function App() {
-
-    console.log()
 
     return (
         <AuthProvider>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,35 @@
 import axios from "axios";
+import test from '../firebase';
+
+// Initialize an instance of axios, pass in the header type.
+const firebase = test.firebase_;
+const authorize = axios.create();
+
+// Observing firebase.auth() to listen signin/signout.
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log("[API] User is signed in");
+        // Getting an authorization token from Fireback to send to the backend.
+        firebase.auth().currentUser.getIdToken(true).then(function (idToken) {
+            // Intercepting any request and appending token to the header.
+            authorize.interceptors.request.use(function (config) {
+                config.headers.authorization = idToken;
+                return config;
+            });  
+        }).catch(function (error) {
+            console.log("[API] Error:", error);
+        });
+    } else {
+        console.log("[API] User is not signed in");
+    }
+});
 
 // The getRecipes method retrieves recipes from the server
 export default {
-  getRecipes: function() {
-    return axios.get("/api/recipes");
-  },
-  getIngredients: function() {
-    return axios.get("/api/Ingredients");
-  }
+    getRecipes: function () {
+        return authorize.get("/api/recipes");
+    },
+    getIngredients: function () {
+        return authorize.get("/api/Ingredients");
+    }
 };
