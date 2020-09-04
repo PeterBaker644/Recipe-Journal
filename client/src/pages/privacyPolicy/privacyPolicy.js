271c77c630225aefba0416cda7410b795f9f35e6
Chue Yang <chueyangib@gmail.com>
2020-09-03 22:31:35 -0500

    linked privacy policy and css border

--- client/src/pages/privacyPolicy/privacyPolicy.js
+++ client/src/pages/privacyPolicy/privacyPolicy.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ".privacyPolicy.css";
+import "./privacyPolicy.css";
 
 function PrivacyPolicy() {
   return (
-    <div>
-      <h1 className="font-brand">Privacy Policy</h1>
+    <div className="container">
+      <h1>Privacy Policy</h1>
       <p>Last updated: September 03, 2020</p>
       <p>
         This Privacy Policy describes Our policies and procedures on the
@@ -25,15 +25,15 @@ function PrivacyPolicy() {
         </a>
         .
       </p>
-      <h1>Interpretation and Definitions</h1>
-      <h2>Interpretation</h2>
+      <h2>Interpretation and Definitions</h2>
+      <h3>Interpretation</h3>
       <p>
         The words of which the initial letter is capitalized have meanings
         defined under the following conditions. The following definitions shall
         have the same meaning regardless of whether they appear in singular or
         in plural.
       </p>
-      <h2>Definitions</h2>
+      <h3>Definitions</h3>
       <p>For the purposes of this Privacy Policy:</p>
       <ul>
         <li>
@@ -123,9 +123,9 @@ function PrivacyPolicy() {
           </p>
         </li>
       </ul>
-      <h1>Collecting and Using Your Personal Data</h1>
-      <h2>Types of Data Collected</h2>
-      <h3>Personal Data</h3>
+      <h2>Collecting and Using Your Personal Data</h2>
+      <h3>Types of Data Collected</h3>
+      <h4>Personal Data</h4>
       <p>
         While using Our Service, We may ask You to provide Us with certain
         personally identifiable information that can be used to contact or
@@ -140,7 +140,7 @@ function PrivacyPolicy() {
           <p>Usage Data</p>
         </li>
       </ul>
-      <h3>Usage Data</h3>
+      <h4>Usage Data</h4>
       <p>Usage Data is collected automatically when using the Service.</p>
       <p>
         Usage Data may include information such as Your Device's Internet
@@ -162,7 +162,7 @@ function PrivacyPolicy() {
         visit our Service or when You access the Service by or through a mobile
         device.
       </p>
-      <h3>Tracking Technologies and Cookies</h3>
+      <h4>Tracking Technologies and Cookies</h4>
       <p>
         We use Cookies and similar tracking technologies to track the activity
         on Our Service and store certain information. Tracking technologies used
@@ -238,7 +238,7 @@ function PrivacyPolicy() {
         cookies, please visit our Cookies Policy or the Cookies section of our
         Privacy Policy.
       </p>
-      <h2>Use of Your Personal Data</h2>
+      <h4>Use of Your Personal Data</h4>
       <p>The Company may use Personal Data for the following purposes:</p>
       <ul>
         <li>
@@ -313,7 +313,7 @@ function PrivacyPolicy() {
           profile.
         </li>
       </ul>
-      <h2>Retention of Your Personal Data</h2>
+      <h4>Retention of Your Personal Data</h4>
       <p>
         The Company will retain Your Personal Data only for as long as is
         necessary for the purposes set out in this Privacy Policy. We will
@@ -329,7 +329,7 @@ function PrivacyPolicy() {
         functionality of Our Service, or We are legally obligated to retain this
         data for longer time periods.
       </p>
-      <h2>Transfer of Your Personal Data</h2>
+      <h4>Transfer of Your Personal Data</h4>
       <p>
         Your information, including Personal Data, is processed at the Company's
         operating offices and in any other places where the parties involved in
@@ -349,21 +349,21 @@ function PrivacyPolicy() {
         a country unless there are adequate controls in place including the
         security of Your data and other personal information.
       </p>
-      <h2>Disclosure of Your Personal Data</h2>
-      <h3>Business Transactions</h3>
+      <h3>Disclosure of Your Personal Data</h3>
+      <h4>Business Transactions</h4>
       <p>
         If the Company is involved in a merger, acquisition or asset sale, Your
         Personal Data may be transferred. We will provide notice before Your
         Personal Data is transferred and becomes subject to a different Privacy
         Policy.
       </p>
-      <h3>Law enforcement</h3>
+      <h4>Law enforcement</h4>
       <p>
         Under certain circumstances, the Company may be required to disclose
         Your Personal Data if required to do so by law or in response to valid
         requests by public authorities (e.g. a court or a government agency).
       </p>
-      <h3>Other legal requirements</h3>
+      <h4>Other legal requirements</h4>
       <p>
         The Company may disclose Your Personal Data in the good faith belief
         that such action is necessary to:
@@ -380,7 +380,7 @@ function PrivacyPolicy() {
         </li>
         <li>Protect against legal liability</li>
       </ul>
-      <h2>Security of Your Personal Data</h2>
+      <h3>Security of Your Personal Data</h3>
       <p>
         The security of Your Personal Data is important to Us, but remember that
         no method of transmission over the Internet, or method of electronic
@@ -388,7 +388,7 @@ function PrivacyPolicy() {
         means to protect Your Personal Data, We cannot guarantee its absolute
         security.
       </p>
-      <h1>Your California Privacy Rights (California's Shine the Light law)</h1>
+      <h2>Your California Privacy Rights (California's Shine the Light law)</h2>
       <p>
         Under California Civil Code Section 1798 (California's Shine the Light
         law), California residents with an established business relationship
@@ -401,10 +401,10 @@ function PrivacyPolicy() {
         Light law, and if you are a California resident, You can contact Us
         using the contact information provided below.
       </p>
-      <h1>
+      <h2>
         California Privacy Rights for Minor Users (California Business and
         Professions Code Section 22581)
-      </h1>
+      </h2>
       <p>
         California Business and Professions Code section 22581 allow California
         residents under the age of 18 who are registered users of online sites,
@@ -421,7 +421,7 @@ function PrivacyPolicy() {
         removal of content or information posted online and that the law may not
         permit or require removal in certain circumstances.
       </p>
-      <h1>Links to Other Websites</h1>
+      <h2>Links to Other Websites</h2>
       <p>
         Our Service may contain links to other websites that are not operated by
         Us. If You click on a third party link, You will be directed to that
@@ -432,7 +432,7 @@ function PrivacyPolicy() {
         We have no control over and assume no responsibility for the content,
         privacy policies or practices of any third party sites or services.
       </p>
-      <h1>Changes to this Privacy Policy</h1>
+      <h2>Changes to this Privacy Policy</h2>
       <p>
         We may update our Privacy Policy from time to time. We will notify You
         of any changes by posting the new Privacy Policy on this page.
@@ -447,7 +447,7 @@ function PrivacyPolicy() {
         changes. Changes to this Privacy Policy are effective when they are
         posted on this page.
       </p>
-      <h1>Contact Us</h1>
+      {/* <h1>Contact Us</h1>
       <p>
         If you have any questions about this Privacy Policy, You can contact us:
       </p>
@@ -462,7 +462,7 @@ function PrivacyPolicy() {
             http://www.receipebox.com/contact
           </a>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
--- client/src/pages/splash/Splash.js
+++ client/src/pages/splash/Splash.js
@@ -21,6 +21,9 @@ function Splash() {
                 <Link to="/login" className="btn-secondary rb-btn">Sign In</Link>
                 <Link to="/signup" className="btn-secondary rb-btn mx-4">Sign Up</Link>
                 <Link to="/Gene" className="btn-secondary rb-btn">Gene's Page</Link>
+                <Link to="/recipeBox" className="btn-secondary rb-btn">Jacob's</Link>
+                <Link to="/create" className="btn-secondary rb-btn">Create Recipe</Link>
+                <Link to="/privacy" className="btn-secondary rb-btn">Privacy Policy</Link>
                 {!!currentUser ? <button className="rb-btn btn-primary ml-4" onClick={() => app.auth().signOut()}>Sign Out</button> : ""}
             </section>
         </TestCard>
