e857f83b2f339c3c483afcc7d595cce597722227
Blackstar <baker644@umn.edu>
2020-09-10 19:23:29 -0500

    More testing

---
+++ client/build/precache-manifest.406eafeecf076abe02576754631ef320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3150d7514e8639a7878d03a3bf77cd2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaddb65357ae9cc04dc8",
+    "url": "/static/css/main.d2fbac9c.chunk.css"
+  },
+  {
+    "revision": "2375bed42067d9170ec2",
+    "url": "/static/js/2.25d7832c.chunk.js"
+  },
+  {
+    "revision": "4c28af4c96a704b112cc4965f029f199",
+    "url": "/static/js/2.25d7832c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaddb65357ae9cc04dc8",
+    "url": "/static/js/main.04cce7b3.chunk.js"
+  },
+  {
+    "revision": "49600be04800e9376f0d",
+    "url": "/static/js/runtime-main.86a8f658.js"
+  },
+  {
+    "revision": "7463777c4f2903ca0cb4b4210b62197e",
+    "url": "/static/media/box5.7463777c.png"
+  }
+]);
