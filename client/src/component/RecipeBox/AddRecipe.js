e06effad4905fad5833f724e15b2f149c204b2a2
Jacob Kyle <jkthomps21@gmail.com>
2020-09-11 21:57:14 -0500

    merge master

--- client/src/component/RecipeBox/AddRecipe.js
+++ client/src/component/RecipeBox/AddRecipe.js
@@ -4,11 +4,10 @@ import {Link} from 'react-router-dom';
 
 function AddRecipe() {
     return (
-<<<<<<< HEAD
         <Link to="/create/info" style={{textDecoration: "none"}}>
             <div className="col my-2 h-100 pb-3 font-book">
                 <div className="card h-100 recipe-card">
-                    <div className="card-body d-flex align-items-center justify-content-center">
+                    <div className="card-body d-flex align-items-center justify-content-center btn-add">
                         {/* Below link needs to redirect to Chue's page */}
                         <AddBtn/>
                     </div>
@@ -17,13 +16,6 @@ function AddRecipe() {
                             Add a Recipe!
                         </span>
                     </div>
-=======
-        <Link to="/create/info">
-            <div className="card h-100 recipe-card">
-                <div className="card-body d-flex align-items-center justify-content-center btn-add">
-                    {/* Below link needs to redirect to Chue's page */}
-                    <AddBtn/>
->>>>>>> origin
                 </div>
             </div>
         </Link>
